2c8d8c9475484bb6c1e026e65e0ad8dd276b7731
Eraze86 <therese.rosvall@hotmail.se>
2022-03-17 11:22:26 +0100

    render list

--- src/components/render/render.tsx
+++ src/components/render/render.tsx
@@ -1,4 +1,22 @@
+import { IPerson } from "./IPerson"
+
 export function Render(){
-    let name = "Therese"
-return({name})
+    let persons = [
+        {name: "Anna", age: 20, married: false},
+        {name: "Peter", age: 35, married: true},
+        {name: "Lena", age: 16, married: false},
+        {name: "Anders", age: 65, married: true}
+    ]
+    
+   let list = persons.map((p) =>{
+    if(p.married === true){
+        return <li key={p.name}>{p.name} {p.age} {p.married}, is married</li>
+    }else{
+        return <li key={p.name}>{p.name} {p.age} {p.married}, is not married</li>
+    }
+      
+
+   })
+  
+return(<ul>{list}</ul>)
 }
